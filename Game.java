c4c1792b1f939ff38a3012498c2061378de4ea39
kutaybalta <kutaybalta@yahoo.com>
2023-07-19 13:45:02 +0300

    Game mechanics first implementations, new classes

---
+++ Game.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private int playerCount;
+    public boolean hasSomeoneWon = false;
+    private int currentPlayerIndex = 0;
+    private List<Player> players = new ArrayList<>();
+    private List<Card> deck = new ArrayList<>();
+    private List<Card> cardsInMiddle = new ArrayList<>();
+
+    public Game(int playerCount) {
+        this.playerCount = playerCount;
+        this.deck = Card.getStandartDeck();
+        for (int i = 0; i < playerCount; i++) {
+            players.add(Player.createPlayer());
+            for (int j = 0; j < 7; j++) {
+                players.get(i).getCards().add(Card.getRandomCardFromDeck(this.deck));
+            }
+        }
+        Card firstCard;
+        do {
+            firstCard = Card.getRandomCardFromDeck(this.deck);
+            cardsInMiddle.add(firstCard);
+        }while (firstCard.type() != Card.Ctype.NORMAL);
+    }
+
+}
---
+++ Player.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private List<Card> playerCards = new ArrayList<>();
+    private int drawCount = 0;
+    private boolean hadDrawOneCard = false;
+
+    public static Player createPlayer() {
+        return new Player();
+    }
+
+    public List<Card> getCards() {
+        return playerCards;
+    }
+}
